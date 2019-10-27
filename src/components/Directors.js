6ccd4239df060c48cc6464b9cbc1f8885c1112f7
Alan Krajina <alankrajina88@gmail.com>
2019-10-27 12:35:38 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,12 +5,12 @@ const Directors = () => {
   return (
     <div>
         <h1>Directors Page</h1>
-        
-        {directors.map(director=> 
+
+        {directors.map(director=>
         <div>
           <h3>Name: {director.name}</h3>
           <p>Movies: </p>
-            <ul>  
+            <ul>
               {director.movies.map(movie=><li>{movie}</li>)}
             </ul>
         </div>
